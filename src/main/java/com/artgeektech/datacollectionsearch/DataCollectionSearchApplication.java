41af4822bc744cf7d3793cecf662112112119bae
ArtGeekTech <agt@ArtGeekTechs-Mac-mini.local>
2020-12-29 15:43:40 -0800

    clean slate

---
+++ src/main/java/com/artgeektech/datacollectionsearch/DataCollectionSearchApplication.java
@@ -0,0 +1,13 @@
+package com.artgeektech.datacollectionsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollectionSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCollectionSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artgeektech/datacollectionsearch/DataCollectionSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artgeektech.datacollectionsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCollectionSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
